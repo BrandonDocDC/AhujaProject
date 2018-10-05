17ef763a9e52dfd13b2029401adc13dfc011fb6e
BrandonDocDC <32312852+BrandonDocDC@users.noreply.github.com>
2018-10-05 00:27:18 -0400

    Update and rename multiserver.java to server.java
    cleaned up server file. commented out threading for server, runs iterative per client connection/request.
    ## project 2 will create threads for each connection and run the requests in parallel.

--- server.java
+++ server.java
@@ -1,58 +1,67 @@
-package multiserver;
+package server;
 
-//Imports 
-import java.io.*;
-import java.net.*;
+//Imports
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 /* Project 1 - Networks and Distributed Systems
 /* Dr. Ahuja
 /* Brandon DeCrescenzo, Kristoffer Binek, Nahjani Rhymer
 */
 
-public class MultiServer {
-
+// main server file. to compile: "javac server.java"; to run: "java server <port>"
+public class Server {
+    
     public static void main(String[] args) throws IOException {
         //Variables 
         int portNumber = Integer.parseInt(args[0]);
         //used in project 2 for ThreadedServer
         //int numThreads = Integer.parseInt(args[1]);
         //MultiThread[] clientThreads = new MultiThread[numThreads];
-
+        
         //ERROR: If hostName or portNumber is wrong
         if (args.length < 1) {
-            System.err.println("Usage: java MultiServer <port number>");
+            System.err.println("Usage: java server <port number>");
             System.exit(1);
         }
         
         //If port number is correct, proceed...
         try {
-            //Create a server 
+            //Create a server
             ServerSocket serverSocket = new ServerSocket(portNumber);
             System.out.println("Server started... Listening on port " + portNumber);
             System.out.println("Waiting for clients...");
             
+            // this is project 1, unthreaded server responses
+            client[i] = new ServerThread(serverSocket.accept());
+            client[i].start();
+			
+            // this is project 2, threaded server responses
             //Create clients threads in the background
             //for(int i = 0; i > numThreads; i++)
             //{
-                clientThreads[i] = new MultiThread(serverSocket.accept());
+            //    clientThreads[i] = new MultiThread(serverSocket.accept());
             //}
             //for(int i = 0; i > numThreads; i++)
             //{
-                clientThreads[i].start();
+            //    clientThreads[i].start();
             //}
 
-            //Keep server open to accept multiple clients 
+            //Keep server open to accept multiple clients
             while (true) {
-                //Create a thread 
-                new MultiThread(serverSocket.accept()).start();
-            }//End while 
+                //Create a thread
+                new ServerThread(serverSocket.accept()).start();
+            }//End while
             
-        }//End try 
+        }//End try
         
-        //End catches 
+        //End catches
         catch (IOException e) {
             System.out.println("Exception caught..." + e);
-        }//End catch 
-
-    }//End main 
-}//End MultiSever 
+        }//End catch
+        
+    }//End main
+}//End Sever
