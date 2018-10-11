223ed4278bae74aef78dac732f0bd9cb4ab83842
BrandonDocDC <32312852+BrandonDocDC@users.noreply.github.com>
2018-10-11 13:15:41 -0400

    Update Client.java

--- V3/Client.java
+++ V3/Client.java
@@ -31,6 +31,7 @@ public class Client {
             Socket clientSocket = new Socket(hostName, portNumber);
       
             //While the socket is open, it will listen for host response and display menu after
+			//while1
 			while(true){  
            
 				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); // doorstop to keep listening on socket
@@ -38,78 +39,82 @@ public class Client {
 				BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in)); //user menu input
 				Scanner s = new Scanner(System.in); // s var to loop executions
 
-				long start_time; 
-				 //log print line, broken up to show output (menu)
-				System.out.println( "1) Host Current Date and Time\n"
-						          + "2) Host Current Uptime\n"
-						          + "3) Host Current Memory Use\n"
-						          + "4) Host Current Netstat\n"
-						          + "5) Host Current Users\n"
-						          + "6) Host Running Processes\n"
-						          + "7) Quit\n" );
-								  
-				System.out.println("Select your option: ");
-				option = stdIn.read();
+				//while the socket is live, it will keep posting the menu
+				//while2
+				while (true) {
+					long start_time; 
+					 //log print line, broken up to show output (menu)
+					System.out.println( "1) Host Current Date and Time\n"
+									  + "2) Host Current Uptime\n"
+									  + "3) Host Current Memory Use\n"
+									  + "4) Host Current Netstat\n"
+									  + "5) Host Current Users\n"
+									  + "6) Host Running Processes\n"
+									  + "7) Quit\n" );
+									  
+					System.out.println("Select your option: ");
+					option = stdIn.read();
 
-				// Switch Menu
-				switch (option) {
-					case '1':
-						//start_time = System.currentTimeMillis();
-						//System.out.println("Requesting Time");
-						out.println("1");
-						System.out.println("");
-						System.out.println("The Date and Time: " + in.readLine());
-						break;
-					case '2':
-						out.println("2");
-						System.out.println("");
-						System.out.println("System has been " + in.readLine());
-						break;
-					case '3':
-						out.println("3");
-						System.out.println("");
-						while (in.readLine() != null) {
+					// Switch Menu
+					switch (option) {
+						case '1':
+							//start_time = System.currentTimeMillis();
+							//System.out.println("Requesting Time");
+							out.println("1");
+							System.out.println("");
+							System.out.println("The Date and Time: " + in.readLine());
+							break;
+						case '2':
+							out.println("2");
+							System.out.println("");
+							System.out.println("System has been " + in.readLine());
+							break;
+						case '3':
+							out.println("3");
+							System.out.println("");
+							while (in.readLine() != null) {
+								System.out.println(in.readLine());
+							}
+							break;
+						case '4':
+							out.println("4");
+							System.out.println("");
+							while (in.readLine() != null) {
+								System.out.println(in.readLine());
+							}
+							break;
+						case '5':
+							out.println("5");
+							System.out.println("");
+							System.out.println("Current Users: " + in.readLine());
+							break;
+						case '6':
+							out.println("6");
+							System.out.println("");
+							while (in.readLine() != null) {
+								System.out.println(in.readLine());
+							}
+							break;
+						case '7':
+							out.println("7");
+							System.out.println("");
 							System.out.println(in.readLine());
-						}
-						break;
-					case '4':
-						out.println("4");
-						System.out.println("");
-						while (in.readLine() != null) {
-							System.out.println(in.readLine());
-						}
-						break;
-					case '5':
-						out.println("5");
-						System.out.println("");
-						System.out.println("Current Users: " + in.readLine());
-						break;
-					case '6':
-						out.println("6");
-						System.out.println("");
-						while (in.readLine() != null) {
-							System.out.println(in.readLine());
-						}
-						break;
-					case '7':
-						out.println("7");
-						System.out.println("");
-						System.out.println(in.readLine());
-						return;
-					default:
-						System.err.println("  --  Unrecognized option.");
-						continue;
-				} //end switch
-				System.out.println("\n  --  Request is done");
+							return;
+						default:
+							System.err.println("  --  Unrecognized option.");
+							continue;
+					} //end switch
+					System.out.println("\n  --  Request is done");
 
-				while ((userInput = in.readLine()) != null && !userInput.equalsIgnoreCase("Finished")) {
-					out.println(userInput);
-				}//end while
-				//long end_time = System.currentTimeMillis();
+					while ((userInput = in.readLine()) != null && !userInput.equalsIgnoreCase("Finished")) {
+						out.println(userInput);
+					}//end while
+					//long end_time = System.currentTimeMillis();
 
+				}//end while2
 				//Print length of time and status of option
 				//System.out.println("  --  Completed in " + (end_time-start_time) + "ms");
-			}// end while loop
+			}// end while1
 		}//end try
 
 		//Catchs
