13767c20c15623cd52499735f6020c37bf0f92f6
BrandonDocDC <32312852+BrandonDocDC@users.noreply.github.com>
2018-10-11 21:55:41 -0400

    Update MultiClient.java

--- V3/MultiClient.java
+++ V3/MultiClient.java
@@ -1,8 +1,36 @@
+/** Project 1 - Networks and Distributed Systems
+  * Dr. Ahuja
+  * Brandon DeCrescenzo, Kristoffer Binek, Nahjani Rhymer
+  * MultiClient.java
+*/
+
+//Imports
+import java.io.*;
+import java.util.*;
+
+/** This is the threaded Load Test Client. You will select the number for the request &
+ *  how many threads you would like. The client will create the threads, connect the
+ *  threads and send the request. Then server will respond to the requests (unthreaded 
+ *  will respond iteratively, project 2 concurrently). It will send the response back
+ *  then the client will process the times for each one.
+ *
+ *  (starts time) (Client: builds the array, sends it) (Server: receives the array, processes it, sends array back
+ *  (Client: receives it, processes the array)(ends time)
+ */
+ 
+// client threaded class: main method will call all methods to:
+// create the socket, create the threads, and connect the threads. everything else is in run()
+
+public class MultiClient {
+	static String hostName;
+	static int portNumber;
+	static int menuSelected;
+	
 	//main method
 	public static void main(String[] args) throws IOException, InterruptedException {
 		// check the args to ensure the correct number of args are given
 		if(args.length < 2) {
-			System.out.println("Please enter arguments in this format java ClientThreaded <server IP Address> <port number>\n");
+			System.out.println("Please enter arguments in this format java MultiClient <server IP Address> <port number>\n");
 			System.exit(0);
 		}// end if args.length < 2 check
 		hostName = args[0];
@@ -53,7 +81,7 @@
 	public static void runThreads(int times, Thread[] theThreads) {
 		System.out.println("Client threaded " + times + " times.");
 		for(int index1 = 0; index1 < theThreads.length; index1++) {
-			theThreads[index1] = new ClientThreaded(hostName, portNumber, menuSelected);
+			theThreads[index1] = new MultiClient(hostName, portNumber, menuSelected);
 		}
 		for(int index = 0; index < theThreads.length; index++) {
 			
@@ -61,3 +89,11 @@
 			theThreads[index].run();
 		}// end runThreads method
 	}// end runThreads method
+
+	//MultiClient Method
+	public MultiClient(String host, int portNumber, int menuItem) {
+		hostName = host;
+		port = portNumber;
+		option = menuItem;
+	}// end MultiClient constructor
+}// end MultiClient class
