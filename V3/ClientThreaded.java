d08ee7919f56e9822e6fea42558c6f9b8382726b
BrandonDocDC <32312852+BrandonDocDC@users.noreply.github.com>
2018-10-11 21:47:38 -0400

    Create ClientThreaded.java

---
+++ V3/ClientThreaded.java
@@ -0,0 +1,63 @@
+	//main method
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// check the args to ensure the correct number of args are given
+		if(args.length < 2) {
+			System.out.println("Please enter arguments in this format java ClientThreaded <server IP Address> <port number>\n");
+			System.exit(0);
+		}// end if args.length < 2 check
+		hostName = args[0];
+		
+		try {
+			portNumber = Integer.parseInt(args[1]);
+		}// end try
+		catch (NumberFormatException e) {
+			System.out.println("User invalid input, please enter a port number as an Integer.");
+			System.exit(-1);
+		}// end catch (NumberFormatException)
+		while(true) {
+		menu();
+		System.out.println("How many threads:");
+		Scanner keyboard = new Scanner(System.in);
+		int numberOfTimes = keyboard.nextInt();
+		Thread[] theThreads = new Thread[numberOfTimes];
+		//runThreads(numberOfTimes, theThreads, hostName, portNumber);
+		runThreads(numberOfTimes, theThreads);
+		for(int index = 0; index < numberOfTimes; index++) 
+			theThreads[index].join();
+		}
+	}// end main method 
+	
+	//menu method
+	public static void menu() {
+		System.out.println( "1) Host Current Date and Time\n"
+						  + "2) Host Uptime\n"
+						  + "3) Host Memory Use\n"
+						  + "4) Host Netstat\n"
+						  + "5) Host Current Users\n"
+						  + "6) Host Running Processes\n"
+						  + "7) Quit\n" );
+		System.out.println("Select your option: ");
+		Scanner sc = new Scanner(System.in);
+		while(!sc.hasNextInt())
+		{
+			System.out.println("User invalid input, input number between 1 or 7");
+		        sc.next();
+		}// end while !sc.hasNextInt loop
+		menuSelected = sc.nextInt();
+		System.out.println("======================================================");
+		
+	}// end menu method
+	
+	//runThreads mathod
+	//public static void runThreads(int times, Thread[] theThreads, string hostName, int portNumber) {
+	public static void runThreads(int times, Thread[] theThreads) {
+		System.out.println("Client threaded " + times + " times.");
+		for(int index1 = 0; index1 < theThreads.length; index1++) {
+			theThreads[index1] = new ClientThreaded(hostName, portNumber, menuSelected);
+		}
+		for(int index = 0; index < theThreads.length; index++) {
+			
+			System.out.println("Thread_" + (index + 1));
+			theThreads[index].run();
+		}// end runThreads method
+	}// end runThreads method
