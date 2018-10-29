9cc3819630aacbe1dbac6feff7164ab5ff493964
BrandonDocDC <32312852+BrandonDocDC@users.noreply.github.com>
2018-10-29 17:00:04 -0400

    Update MultiServer.java

--- V4/MultiServer.java
+++ V4/MultiServer.java
@@ -1,4 +1,4 @@
-/** Project 1 - Networks and Distributed Systems
+/** Project 2 - Networks and Distributed Systems
  * Dr. Ahuja
  * Brandon DeCrescenzo, Kristoffer Binek, Nahjani Rhymer
  * MultiServer.java
